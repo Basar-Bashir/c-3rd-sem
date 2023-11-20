d312c1ab9bf6be162fc124ef880b85eae5edde6e
Basar Bashir <134760873+Basar-Bashir@users.noreply.github.com>
2023-11-20 20:30:27 +0530

    Add files via upload

---
+++ gallon to feet cube.cpp
@@ -0,0 +1,11 @@
+#include<Iostream>
+using namespace std;
+int main()
+{
+    float g,f;
+    cout<<"Enter the number of gallons.";
+    cin>>g;
+    f=g/7.481;
+    cout<<"The equivalent in cubic feet for given gallons is "<<f;
+    return 0;
+}
